1a8bcf65e4037356f657a14b447beb4e9b4af8f9
Enrique Fernandez <enrique.fernandez@kivra.com>
2022-04-22 11:20:13 +0200

    Run compaction on provided vnode

--- src/main.cpp
+++ src/main.cpp
@@ -25,42 +25,35 @@ int main(int argc, char* argv[]) {
 	return 0;
     }
 
-    fs::directory_iterator end_iter;
-    for (fs::directory_iterator dir_itr(p);
-         dir_itr != end_iter;
-         ++dir_itr) {
-        if (fs::is_directory(dir_itr->status())) {
-            std::cout << dir_itr->path() << " [directory]\n";
+    std::cout << p.c_str() << " [directory]\n";
 
-            leveldb::DB *db;
+    leveldb::DB *db;
 
-            // Open the database with the specified "name".
-            // Stores a pointer to a heap-allocated database in *dbptr and returns
-            // OK on success.
-            // Stores NULL in *dbptr and returns a non-OK status on error.
-            // Caller should delete *dbptr when it is no longer needed.
-            leveldb::Status status = leveldb::DB::Open(options, dir_itr->path().c_str(), &db);
-            if (status.ok()) {
-                std::cout << "compacting..." << std::endl;
+    // Open the database with the specified "name".
+    // Stores a pointer to a heap-allocated database in *dbptr and returns
+    // OK on success.
+    // Stores NULL in *dbptr and returns a non-OK status on error.
+    // Caller should delete *dbptr when it is no longer needed.
+    leveldb::Status status = leveldb::DB::Open(options, p.c_str(), &db);
+    if (status.ok()) {
+      std::cout << "compacting..." << std::endl;
 
-                // Compact the underlying storage for the key range [*begin,*end].
-                // In particular, deleted and overwritten versions are discarded,
-                // and the data is rearranged to reduce the cost of operations
-                // needed to access the data.  This operation should typically only
-                // be invoked by users who understand the underlying implementation.
-                //
-                // begin==NULL is treated as a key before all keys in the database.
-                // end==NULL is treated as a key after all keys in the database.
-                // Therefore the following call will compact the entire database:
-                //    db->CompactRange(NULL, NULL);
-                db->CompactRange(NULL, NULL);
-                delete db;
+      // Compact the underlying storage for the key range [*begin,*end].
+      // In particular, deleted and overwritten versions are discarded,
+      // and the data is rearranged to reduce the cost of operations
+      // needed to access the data.  This operation should typically only
+      // be invoked by users who understand the underlying implementation.
+      //
+      // begin==NULL is treated as a key before all keys in the database.
+      // end==NULL is treated as a key after all keys in the database.
+      // Therefore the following call will compact the entire database:
+      //    db->CompactRange(NULL, NULL);
+      db->CompactRange(NULL, NULL);
+      delete db;
 
-                std::cout << "done" << std::endl;
-            } else {
-                std::cout << "Unable to open " << dir_itr->path() << ", error: " << status.ToString() << std::endl;
-            }
-        }
+      std::cout << "done" << std::endl;
+    } else {
+      std::cout << "Unable to open " << p.c_str();
     }
 
     return 0;
